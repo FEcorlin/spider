aa3d54d16da447f46a8c2ed900b31fb693c18a42
chen jun <chenjun@chendeMacBook-Pro.local>
2019-02-15 23:32:19 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,13 @@ var https = require('https');
 var cheerio = require('cheerio');
 var request = require('request');
 var db = require('../config/mysql_pool');
+var fs = require('fs');
 
 
-var url = "http://weixin.sogou.com";//首页地址
+var url = "http://www.526tu.com/htm/mp43/{number}.htm"; //首页地址
+var _begin = 2346; // 开始 23317
+var _end = 2857; // 结束2857
 
-//http://weixin.sogou.com/pcindex/pc/pc_0/pc_0.html   第1页
-//http://weixin.sogou.com/pcindex/pc/pc_0/1.html    第2页
-// pc_0  代表第一个分类
-// 一直到pc_21
-
-var art_lists_url = [];//文章地址列表
-var i = 0,j= 1,li_i=0;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -29,17 +25,15 @@ router.get('/start', function (req, res, next) {
 });
 
 function startFetch(){
-    i = 0;
     console.log('==========start================');
-    getFirstList(i);
+    getFirstList();
 }
 
 //获取一级url列表
-function getFirstList(index) {
-    var list_url = url+'/pcindex/pc/pc_'+index+'/pc_'+index+'.html';
+function getFirstList() {
+    var list_url = url.replace('{number}', _begin);
+
     //采用http模块向服务器发起一次get请求
-    console.log(list_url);
-    console.log(art_lists_url.length);
     http.get(list_url, function (res) {
         var html = '';        //用来存储请求网页的整个html内容
         res.setEncoding('utf-8'); //防止中文乱码
@@ -50,19 +44,20 @@ function getFirstList(index) {
         //监听end事件，如果整个网页内容的html都获取完毕，就执行回调函数
         res.on('end', function () {
             var $ = cheerio.load(html);
-            if ($(".news-list").length > 0) {//判断是否还有内容
-                $(".news-list li").each(function (index, item) {
-                    art_lists_url.push($(this).find('.txt-box').find('h3').find('a').attr('href'));
-                });
-                getSecList(j);
-            }
-            else {
-                getContent(art_lists_url[li_i]);//列表爬完，那链接去取内容
+            var _url = $(".downurl a").text() || $(".downurl").html() || '';
+            console.log(_url);
+            fs.appendFile('./public/result.text', _url + '\r');
+            if (_begin < _end) { //下一个地址
+                _begin ++;
+                getFirstList();
+            } else {
+                console.log('======end==========');
+                return true;
             }
         });
     });
 }
-//获取二级url列表，相当于一级的下一页
+// //获取二级url列表，相当于一级的下一页
 function getSecList(index){
     var list_url = url+'/pcindex/pc/pc_'+i+'/'+index+'.html';
     console.log(list_url);
